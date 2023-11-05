36385190d6b40ad858cb8a1bfa4b6afad2f23c94
Michal Trubini <mchl.trubini@gmail.com>
2023-11-05 18:34:58 +0100

    assets

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
+<template></template>
 
-<template>
+<script></script>
 
-</template>
-
-<script>
-
-</script>
-
-
-<style >
-
+<style>
 body {
-  font-family: 'Manrope', sans-serif;
+  font-family: "Manrope", sans-serif;
 }
+
+p {
+  font-weight: 500;
+  font-size: 0.9375rem;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+h1, h2, h3 {
+  font-weight: 700;
+}
+
 </style>
